9bd9a7c798001dfc8309f39da97329f3a47da63a
Adil Khan <adil.khan@outlook.com>
2019-10-01 18:59:44 -0400

    update mix

--- mix.exs
+++ mix.exs
@@ -41,8 +41,10 @@ defmodule PhxGraphql.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:absinthe_ecto, "~> 0.1.0"},
-      {:absinthe_plug, "~> 1.4"},
+#      {:absinthe_ecto, "~> 0.1.0"},
+#      {:absinthe_plug, "~> 1.4"},
+      {:absinthe, "~> 1.4.0"},
+      {:absinthe_phoenix, "~> 1.4.0"},
       {:poison, "~> 2.1.0"},
       {:absinthe, "~> 1.4"}
     ]
